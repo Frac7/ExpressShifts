862eedc54799d656ad1ae0f0deb26973aa36d8b8
Frac7 <francescacella96@gmail.com>
2020-12-19 22:25:20 +0100

    Details route added

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const products = require('./products');
 
 router.get('/products', products.list);
+router.get('/products/:id', products.details);
 
 module.exports = router;
--- routes/products/index.js
+++ routes/products/index.js
@@ -1,10 +1,19 @@
-const product = require('../../models/product')
+const Products = require('../../models/product');
 
 module.exports = {
 	list: async (req, res) => {
 		try {
-			const products = await product.find();
+			const products = await Products.find();
 			res.json(products);
+		} catch (err) {
+			res.status(500).json({ message: err.message });
+		}
+	},
+	details: async (req, res) => {
+		const id = req.params.id;
+		try {
+			const product = await Products.findById(id);
+			res.json(product);
 		} catch (err) {
 			res.status(500).json({ message: err.message });
 		}
