62f1a53782859d3e4e96720018e96a1f437432f1
Frac7 <francescacella96@gmail.com>
2020-12-19 23:36:02 +0100

    Get shift route improved

--- routes/shifts/index.js
+++ routes/shifts/index.js
@@ -3,10 +3,9 @@ const Shifts = require('../../models/shift');
 module.exports = {
 	get: async (req, res) => {
 		try {
-			const person = req.params.person;
 			const product = req.params.product;
 
-			const shift = await Shifts.findOne({ person, product }).populate('person').populate('product');
+			const shift = await Shifts.findOne({ product }).populate('person').populate('product');
 			res.json(shift);
 		} catch (err) {
 			res.status(500).json({ message: err.message });
